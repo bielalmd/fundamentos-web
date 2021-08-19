a40d194725b43b8049affe0bd0ada6d8acd16d19
Gabriel Almeida <86261232+bielalmd@users.noreply.github.com>
2021-08-18 21:55:22 -0300

    js implementado em Contato

--- assets/script.js
+++ assets/script.js
@@ -10,6 +10,10 @@
 let nome = window.document.getElementById('nome')
 let email = document.querySelector('#email')
 let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+let mapa = document.querySelector('#mapa')
 
 nome.style.width = '100%'
 email.style.width = '100%'
@@ -19,12 +23,15 @@ function validaNome() {
 
     if (nome.value.length <= 2) {
         txtNome.innerHTML = "Nome invalido"
-        txt.style.color = 'red'
+        txtNome.style.color = 'red'
+        
 
     } else {
 
         txtNome.innerHTML = 'Nome válido'
         txtNome.style.color = 'green'
+        nomeOk = true
+        
     }
 }
 
@@ -34,11 +41,54 @@ function validaEmail() {
     if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
         txtEmail.innerHTML = 'Email inválido'
         txtEmail.style.color = "red"
+        
 
     } else {
 
         txtEmail.innerHTML = "Email válido"
         txtEmail.style.color = 'green'
+        emailOk = true
     }
 }
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#txtAssunto')
+    
+
+    if(assunto.value.length >= 100) {
+        txtAssunto.innerHTML = 'Texto muito grande!'
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+        
+    } else{
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+
+    }
+}
+function enviar() {
+    if (nomeOk == true && emailOk == true && assuntoOk){
+        alert ('Formulario enviado com sucesso!')
+    }else{
+        alert ('Formulario incorreto!')
+    }
+}
+
+function mapaZoom() {
+
+    mapa.style.width = '700px'
+    mapa.style.height = '500px'
+
+}
+
+function mapaNormal() {
+
+    mapa.style.width = '350px'
+    mapa.style.height = '250px'
+
+}
+
+
+
+  
 
